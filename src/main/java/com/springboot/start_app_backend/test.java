8d9c6a4f9eab5490371736b95f10bd1a7a67f3be
akram-95 <akramchorfi363@gmail.com>
2022-01-25 06:00:38 +0100

    sd

--- src/main/java/com/springboot/start_app_backend/test.java
+++ src/main/java/com/springboot/start_app_backend/test.java
@@ -3,6 +3,7 @@ package com.springboot.start_app_backend;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.hibernate.internal.ExceptionConverterImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,12 +23,16 @@ public class test {
 	UserProfileRepository userProfileRepository;
 	@GetMapping
 	public String getAllUsers() {
+		try {
 		UserProfile  user= new UserProfile("aaaa12", null, null, null, null, null);
 		Set<Experience> x = new HashSet<>();
 		x.add(new Experience("aa", "bb", "cc", "dd", "ass"));
 		user.setExperiences(x);
 		userProfileRepository.save(user);
 		return "Hello";
+		}catch(Exception e) {
+			return e.getMessage();
+		}
 	}
 
 }
