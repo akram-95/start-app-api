d2d803dfe8d5100868bb87fba0c161cf1fa53b2b
akram-95 <akramchorfi363@gmail.com>
2022-02-11 04:16:06 +0100

    path

--- src/main/java/com/springboot/start_app_backend/controllers/FileStorageController.java
+++ src/main/java/com/springboot/start_app_backend/controllers/FileStorageController.java
@@ -2,6 +2,7 @@ package com.springboot.start_app_backend.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,17 +15,20 @@ import com.springboot.start_app_backend.services.AmazonClient;
 @RestController
 @RequestMapping("/api/storage/aws")
 public class FileStorageController {
-private AmazonClient amazonClient;
-@Autowired
-FileStorageController(AmazonClient amazonClient) {
-this.amazonClient = amazonClient;
-}
-@PostMapping("/uploadFile")
-public String uploadFile(@RequestPart(value = "file") MultipartFile file) throws Exception {
-return this.amazonClient.uploadFile(file);
-}
-@DeleteMapping("/deleteFile")
-public String deleteFile(@RequestParam(value = "url") String fileUrl) throws Exception {
-return this.amazonClient.deleteFileFromS3Bucket(fileUrl);
-}
+	private AmazonClient amazonClient;
+
+	@Autowired
+	FileStorageController(AmazonClient amazonClient) {
+		this.amazonClient = amazonClient;
+	}
+
+	@PostMapping("/uploadFile")
+	public String uploadFile(@RequestPart(value = "file") MultipartFile file) throws Exception {
+		return this.amazonClient.uploadFile(file);
+	}
+
+	@DeleteMapping("/deleteFile/{url}")
+	public String deleteFile(@PathVariable String fileUrl) throws Exception {
+		return this.amazonClient.deleteFileFromS3Bucket(fileUrl);
+	}
 }
