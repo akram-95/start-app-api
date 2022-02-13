9c555b5a98df210ea49b17aed7b5bc214798769a
akram-95 <akramchorfi363@gmail.com>
2022-02-13 01:32:25 +0100

    json Ignore

--- src/main/java/com/springboot/start_app_backend/models/Followers.java
+++ src/main/java/com/springboot/start_app_backend/models/Followers.java
@@ -8,11 +8,13 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 @Entity
 @Table(name = "followers")
 public class Followers {
 
-    public long getId() {
+	public long getId() {
 		return id;
 	}
 
@@ -37,21 +39,24 @@ public class Followers {
 	}
 
 	@Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
-    private long id;
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
 
-    @ManyToOne
-    @JoinColumn(name="from_user_fk")
-    private User from;
+	@ManyToOne
+	@JoinColumn(name = "from_user_fk")
+	@JsonIgnoreProperties({ "following", "followers" })
+	private User from;
 
-    @ManyToOne
-    @JoinColumn(name="to_user_fk")
-    private User to;
+	@ManyToOne
+	@JoinColumn(name = "to_user_fk")
+	@JsonIgnoreProperties({ "following", "followers" })
+	private User to;
 
-    public Followers() {};
+	public Followers() {
+	};
 
-    public Followers(User from, User to) {
-        this.from = from;
-        this.to = to;
-    }
+	public Followers(User from, User to) {
+		this.from = from;
+		this.to = to;
+	}
 }
