32e222c270937d24f0b7514c377e80dd49b30277
akram-95 <akramchorfi363@gmail.com>
2022-02-17 10:53:02 +0100

    asssa

--- src/main/java/com/springboot/start_app_backend/models/Community.java
+++ src/main/java/com/springboot/start_app_backend/models/Community.java
@@ -51,7 +51,7 @@ public class Community {
 
 	// @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 	@JsonIgnoreProperties(value = { "followers", "following" })
-	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@ManyToOne(fetch = FetchType.EAGER, optional = false)
 	@JoinColumn(name = "user_id", nullable = false)
 	@OnDelete(action = OnDeleteAction.CASCADE)
 	private User owner;
