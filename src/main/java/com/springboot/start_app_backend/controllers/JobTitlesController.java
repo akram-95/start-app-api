e2eb4e766f28be5ce57dd4db911d4aa6f33ddbbe
akram-95 <akramchorfi363@gmail.com>
2022-02-21 00:53:57 +0100

    ss

--- src/main/java/com/springboot/start_app_backend/controllers/JobTitlesController.java
+++ src/main/java/com/springboot/start_app_backend/controllers/JobTitlesController.java
@@ -22,7 +22,7 @@ public class JobTitlesController {
 	public String getJobTitlesByTitle(@RequestBody String title) {
 		PageRequest pageRequest = PageRequest.of(0, 20);
 		Page<JobTitles> resultPage = jobTitlesRepository.findByTitleContainingIgnoreCase(title, pageRequest);
-		return "Sized "  + resultPage.getNumberOfElements();
+		return "Sized "  + title;
 
 	}
 
