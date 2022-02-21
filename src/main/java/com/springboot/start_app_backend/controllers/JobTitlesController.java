acb2d36cdd4921584ccb2ec9a5a453a8f929af78
akram-95 <akramchorfi363@gmail.com>
2022-02-21 01:00:40 +0100

    change

--- src/main/java/com/springboot/start_app_backend/controllers/JobTitlesController.java
+++ src/main/java/com/springboot/start_app_backend/controllers/JobTitlesController.java
@@ -19,12 +19,10 @@ public class JobTitlesController {
 	private JobTitlesRepository jobTitlesRepository;
 
 	@GetMapping("/GetByTitle")
-	public String getJobTitlesByTitle(@RequestBody String title) {
-		
+	public Page<JobTitles> getJobTitlesByTitle(@RequestBody String title) {
 		PageRequest pageRequest = PageRequest.of(0, 20);
-		Page<JobTitles> resultPage = jobTitlesRepository.findByTitleContainingIgnoreCase(title, pageRequest);
-		return "Sized "  + title;
-
+		return jobTitlesRepository.findByTitleContainingIgnoreCase(title, pageRequest);
+		
 	}
 
 }
