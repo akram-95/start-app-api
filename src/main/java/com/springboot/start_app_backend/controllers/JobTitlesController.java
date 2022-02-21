a76876f4717b86fab846e00275a309eaa727c47b
akram-95 <akramchorfi363@gmail.com>
2022-02-21 02:02:50 +0100

    ss

--- src/main/java/com/springboot/start_app_backend/controllers/JobTitlesController.java
+++ src/main/java/com/springboot/start_app_backend/controllers/JobTitlesController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,7 +22,7 @@ public class JobTitlesController {
 	@Autowired
 	private JobTitlesRepository jobTitlesRepository;
 
-	@GetMapping("/GetByTitle")
+	@PostMapping("/GetByTitle")
 	public Page<JobTitles> getJobTitlesByTitle(@Valid @RequestBody JobTitles jobTitles) {
 		PageRequest pageRequest = PageRequest.of(0, 20);
 		Page<JobTitles> resultPage = jobTitlesRepository.findByTitleContainingIgnoreCase(jobTitles.getTitle(), pageRequest);
