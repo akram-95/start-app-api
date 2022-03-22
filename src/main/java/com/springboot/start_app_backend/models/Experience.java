b12f2b449d970c05d38e7c82a7aff3fc8acce62b
akram-95 <akramchorfi363@gmail.com>
2022-03-22 02:01:13 +0100

    add JobTypes

--- src/main/java/com/springboot/start_app_backend/models/Experience.java
+++ src/main/java/com/springboot/start_app_backend/models/Experience.java
@@ -1,6 +1,10 @@
 package com.springboot.start_app_backend.models;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -18,10 +22,33 @@ public class Experience {
 	private String description;
 	@Column(name = "valueFrom")
 	private String from;
+
+	public boolean isCurrentlyIn() {
+		return currentlyIn;
+	}
+
+	public void setCurrentlyIn(boolean currentlyIn) {
+		this.currentlyIn = currentlyIn;
+	}
+
+	public Set<String> getJobTypes() {
+		return jobTypes;
+	}
+
+	public void setJobTypes(Set<String> jobTypes) {
+		this.jobTypes = jobTypes;
+	}
+
 	@Column(name = "valueTo")
 	private String to;
+	@Column(name = "currently_in")
+	private boolean currentlyIn;
+	@Column(name = "job_types")
+	@ElementCollection
+	private Set<String> jobTypes = new HashSet<>();
+
 	public Experience() {
-		
+
 	}
 
 	public Integer getId() {
@@ -72,13 +99,16 @@ public class Experience {
 		this.to = to;
 	}
 
-	public Experience(String company, String position, String description, String from, String to) {
+	public Experience(String company, String position, String description, String from, String to, boolean currentyIn,
+			Set<String> jobTypes) {
 		super();
 		this.company = company;
 		this.position = position;
 		this.description = description;
 		this.from = from;
 		this.to = to;
+		this.currentlyIn = currentyIn;
+		this.jobTypes = jobTypes;
 	}
 
 }
